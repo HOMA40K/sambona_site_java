00d60fc3bd98f3982c17b1e1e0ba4a6cbd381669
Arseni <Arsenij522@mail.ru>
2024-06-01 23:39:18 +0300

    Code refactoring
    Refactor code for better work and understanding the code

--- src/main/java/com/example/demo/controllers/Conntroller.java
+++ src/main/java/com/example/demo/controllers/Conntroller.java
@@ -3,42 +3,46 @@ package com.example.demo.controllers;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class Conntroller {
     // RU lang page
-    @GetMapping("/RU/about")
-    public String about(Model model) {
+    @GetMapping("/{lang}/about")
+    public String about(@PathVariable(value = "lang") String lang, Model model) {
+        model.addAttribute("lang", lang);
         model.addAttribute("page_content", "about");
         model.addAttribute("page_text", "This is the about page");
         model.addAttribute("text_1", "lore ipsum");
         return "base";
     }
-    // Change default lang to estonian
-    @GetMapping("/RU")
-    public String home(Model model) {
-        model.addAttribute("page_content", "home");
-        model.addAttribute("page_text", "This is the home page");
-        model.addAttribute("page_lang", "RU");
+    @GetMapping("/{lang}/pricing")
+    public String contact(@PathVariable(value = "lang") String lang, Model model) {
+        model.addAttribute("page_content", "choose-crane");
+        model.addAttribute("page_lang", lang);
+
         return "base";
     }
 
-    @GetMapping("/RU/pricing")
-    public String pricing(Model model) {
+    @GetMapping("/{lang}/home")
+    public String home(@PathVariable(value = "lang") String lang,Model model) {
+        model.addAttribute("page_content", "home");
+        model.addAttribute("page_text", "This is the home page");
+        model.addAttribute("page_lang", lang);
+        return "base";
+    }
+
+    @GetMapping("/{lang}/pricing/{crane_type}")
+    public String contact(@PathVariable(value = "lang") String lang,@PathVariable(value = "crane_type") String crane_type, Model model) {
         model.addAttribute("page_content", "pricing");
-        model.addAttribute("page_text", "This is the pricing page");
-        model.addAttribute("page_lang", "RU");
+        model.addAttribute("crane_type", crane_type);
+        model.addAttribute("page_lang", lang);
         model.addAttribute("text_1", "lore ipsum");
         return "base";
     }
 
-    //EN lang page
-
-    //EE lang page
-
-    //Redirect to default lang page
     @GetMapping("/")
     public String redirectHome(Model model){
-        return "redirect:/RU";
+        return "redirect:/RU/home";
     }
 }
