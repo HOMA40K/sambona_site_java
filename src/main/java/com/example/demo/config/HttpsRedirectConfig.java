fe858f4636eb7c72304e879f2e0dd954bcd0b518
Arseni <Arsenij522@mail.ru>
2024-11-29 15:28:38 +0200

    add redirect https

---
+++ src/main/java/com/example/demo/config/HttpsRedirectConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsRedirectConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return factory -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setScheme("http");
+            connector.setPort(8080); // HTTP port
+            connector.setSecure(false);
+            connector.setRedirectPort(8443); // HTTPS port
+            factory.addAdditionalTomcatConnectors(connector);
+        };
+    }
+}
